113b63dc4c38c2f5363ca2b0057fee658e310e5f
Abdul Waqar <abdulwaqar844@gmail.com>
2026-01-30 18:59:43 +0500

    fixed deps & prisma query

--- schema.py
+++ schema.py
@@ -30,13 +30,14 @@ class Student:
     password_last_change: Optional[str]
     created_at: str
     updated_at: str
+    
 
 
 @strawberry.type
 class Query:
     @strawberry.field
     async def get_student(self, id: strawberry.ID) -> Student:
-        student = await db.Student.find_unique(where={"id": id})
+        student = await db.student.find_unique(where={"id": id})
         
         if student is None:
             raise Exception(f"Student with ID {id} not found")
@@ -46,8 +47,8 @@ class Query:
     @strawberry.field
     async def list_students(self) -> typing.List[Student]:
         try:
-            students = await db.Student.find_many()
-            return [students ]
+            students = await db.student.find_many()
+            return students
         except Exception as e:
             print("DB ERROR:", e)
             raise Exception(f"An error occurred: {str(e)}")
@@ -64,7 +65,7 @@ class StudentInput:
 class Mutation:
     @strawberry.mutation
     async def update_student(self, id: int, input: StudentInput) -> Student:
-        updated_student = await db.Student.update(
+        updated_student = await db.student.update(
             where={"id": id},
             data={
                 "first_name": input.first_name,
@@ -84,11 +85,11 @@ class Mutation:
                 raise ValueError("Password must be at least 8 characters long")
 
             hashed_password = bcrypt.hashpw(input.password.encode('utf-8'), bcrypt.gensalt())
-            existing_student = await db.Student.find_unique(where={"email": input.email})
+            existing_student = await db.student.find_unique(where={"email": input.email})
             if existing_student:
                 raise ValueError(f"Email {input.email} is already registered")
 
-            registered_student  = await db.Student.create(
+            registered_student  = await db.student.create(
                 data={
                     "first_name": input.first_name,
                     "last_name": input.last_name,
@@ -96,6 +97,7 @@ class Mutation:
                     "password": hashed_password.decode('utf-8')
                 }
             )
+            print(registered_student)
             return registered_student
         except ValueError as e:
             # Handle validation errors (like password length or duplicate email)
